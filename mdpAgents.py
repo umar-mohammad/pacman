52deec53e16b814423aa86464b80a9f431dff4de
umar-mohammad <umar.mohammad@kcl.ac.uk>
2021-11-24 22:44:21 +0000

    sg: 16/25, mc: 12/25 exs: 4734

--- mdpAgents.py
+++ mdpAgents.py
@@ -33,40 +33,37 @@ import random
 
 from game import Directions
 
-SMALLGRIDVALUE = {
-        "empty" : 0,
-        "ghost" : -3,
-        "edible_ghost" : 30,
-        "food" : 2,
-        "pacman" : -5,
-        "capsule" : 11,
-        "wall" : "W"
-}
-
-VALUE = {
-        "empty" : -1,
-        "ghost" : -300,
-        "edible_ghost" : 100,
-        "food" : 10,
-        "pacman" : -5,
-        "capsule" : 15,
-        "deathzone" : -20,
-        "wall" : "W"
-}
-
-OBJECTS = dict([(value,obj) for obj, value in VALUE.items()])
-
-
-
 class MDPAgent(game.Agent):
 
     def __init__(self):
         self.map = None
+        self.mapValues = None
 
     def registerInitialState(self, state):
         """
         build initial map to be used for value iteration 
         """
+        smallGridValues = {
+            "empty" : 0,
+            "ghost" : -6,
+            "edible_ghost" : 30,
+            "food" : 3,
+            "pacman" : -5,
+            "capsule" : 11,
+            "wall" : "W"
+        }
+
+        mediumClassicValues = {
+                "empty" : -1,
+                "ghost" : -500,
+                "edible_ghost" : 100,
+                "food" : 10,
+                "pacman" : -5,
+                "capsule" : 15,
+                "deathzone" : -25,
+                "wall" : "W"
+        }
+
         map = []
 
         max_x=0
@@ -75,12 +72,18 @@ class MDPAgent(game.Agent):
         for x,y in api.corners(state):
             max_x = max(x, max_x)
             max_y = max(y, max_y)
+        
+        if max_x == 19 and max_y == 10:
+            # mediumClassic map
+            self.mapValues = mediumClassicValues
+        else:
+            self.mapValues = smallGridValues
 
         for _ in range(max_x+1):
             map.append([0] * (max_y+1))
         
         for x, y in api.walls(state):
-            map[x][y] = VALUE["wall"]
+            map[x][y] = self.mapValues["wall"]
 
         self.map = map
         
@@ -90,10 +93,11 @@ class MDPAgent(game.Agent):
 
     def getAction(self, state):
         legalMoves = api.legalActions(state)
-        val_it_map = self.valueIteration(getRewardMap(state), 0.999)
+        val_it_map = self.valueIteration(self.getRewardMap(state), 0.999)
         max_move = maximisingAction(api.whereAmI(state), legalMoves, val_it_map)
-        # printMap(val_it_map)
+        
         # print('\n' * 5)
+        # printMap(val_it_map)
         # import time
         # time.sleep(3)
 
@@ -121,7 +125,73 @@ class MDPAgent(game.Agent):
             if delta <= 5: 
                 # print(iterations)
                 break
+        # self.iterations += iterations
+        # self.n_vit +=1
+        # self.max_iterations = max(iterations, self.max_iterations)
         self.map = map
+        return map
+    
+    def getRewardMap(self, state):
+        """
+        return a 2D list representing the game state with reward values
+        """
+        # improvement: could make so that the map only checks a few cells in the game grid 
+        # and update instead of rebuilding the whole map over and over
+
+        # improvement: could store and reuse the map array from previous iterations 
+        # so that we dont have to rebuild base array each iteration
+        map = []
+
+        pacman_x, pacman_y = api.whereAmI(state)
+        walls = api.walls(state)
+        corners = api.corners(state)
+        ghosts = api.ghosts(state)
+        edible_ghost = dict(api.ghostStatesWithTimes(state))
+        food = api.food(state)
+        capsules = api.capsules(state)
+
+        max_x = 0
+        max_y = 0
+
+        for x,y in corners:
+            max_x = max(x, max_x)
+            max_y = max(y, max_y)
+
+        # build initial map with empty spaces for every position in grid
+        for _ in range(max_x+1):
+            map.append([self.mapValues["empty"]] * (max_y+1))
+
+        # place all game elements to the map
+        map[pacman_x][pacman_y] = self.mapValues["pacman"]
+        
+        for x,y in capsules:
+            map[x][y] = self.mapValues["capsule"]
+
+        for x,y in food:
+            map[x][y] = self.mapValues["food"]
+        
+        for x, y in walls:
+            map[x][y] = self.mapValues["wall"]
+        
+        for pos in ghosts:
+            # bug : ghosts position can be like x.5, y.5 but pacman's map array does not have indeces such as 1.5 
+            # therefore pacman's internal representation of where the ghosts are might be slightly wrong
+            # and this could lead to bugs/inaccuracies
+            x,y = util.nearestPoint(pos)
+            value = ((self.mapValues["edible_ghost"] * edible_ghost[pos])/40) if pos in edible_ghost and edible_ghost[pos] > 0 else self.mapValues["ghost"]
+
+            map[x][y] += value
+
+            for futureposition, steps in ghostFuture(pos, 4, map):
+                x,y = futureposition
+                map[x][y] += value * (1/steps)
+            
+        if max_x == 19 and max_y == 10:
+            # mediumClassic map
+                for x in range(8,12,1):
+                    map[x][5] += self.mapValues["deathzone"]
+
+
         return map
 
 def getLegalActions(position, map):
@@ -144,33 +214,34 @@ def ghostFuture(position, steps, map):
     """
     returns the position where the ghost can be in future time steps
     """
-    wall = "W"
     x,y = util.nearestPoint(position)
     futurePositions = []
-    for i in range(1, steps+1):
-        if map[x][y+i] != wall:
-            futurePositions.append(((x, y+i), i))
-        else:
-            break
-    
-    for i in range(1, steps+1):
-        if map[x+i][y] != wall:
-            futurePositions.append(((x+i, y), i))
-        else:
-            break
-    
-    for i in range(1, steps+1):
-        if map[x][y-i] != wall:
-            futurePositions.append(((x, y-i), i))
-        else:
-            break
-    
-    for i in range(1, steps+1):
-        if map[x-i][y] != wall:
-            futurePositions.append(((x-i, y), i))
-        else:
-            break
+    futurePositions.extend(ghostFutureHelper((x,y), Directions.NORTH, 0, steps, map))
+    futurePositions.extend(ghostFutureHelper((x,y), Directions.EAST, 0, steps, map))
+    futurePositions.extend(ghostFutureHelper((x,y), Directions.SOUTH, 0, steps, map))
+    futurePositions.extend(ghostFutureHelper((x,y), Directions.WEST, 0, steps, map))
 
+    return futurePositions
+
+def ghostFutureHelper(position, direction, stepstaken, steps, map):
+    wall = "W"
+    x, y = util.nearestPoint(position)
+    futurePositions = []
+    x_add, y_add  = {
+        Directions.NORTH : (0,1),
+        Directions.EAST : (1,0),
+        Directions.SOUTH : (0,-1),
+        Directions.WEST : (-1,0)
+    }[direction]
+
+    for i in range(1, (steps-stepstaken)+1, 1):
+        if map[x+(i*x_add)][y+(i*y_add)] != wall:
+            futurePositions.append(((x+x_add, y+y_add), stepstaken+i))
+            futurePositions.extend(ghostFutureHelper((x+(i*x_add), y+(i*y_add)), Directions.LEFT[direction], stepstaken+i, steps, map))
+            futurePositions.extend(ghostFutureHelper((x+(i*x_add), y+(i*y_add)), Directions.RIGHT[direction], stepstaken+i, steps, map))
+        else:
+            break
+    
     return futurePositions
 
 
@@ -302,7 +373,7 @@ def coordinateAfterMove(position, direction, legalMoves):
     else:
         raise ValueError("direction argument is not a valid Direction")
 
-def printMap(map, humanReadable=False):
+def printMap(map):
     """
     print to terminal a representation of pacman's internal map
     """
@@ -313,65 +384,5 @@ def printMap(map, humanReadable=False):
     for y in range(max_y-1, -1, -1):
         row = ""
         for x in range(max_x):
-            row += OBJECTS[map[x][y]] if humanReadable else (" " + str(int(map[x][y])) + " ") if map[x][y] != "W" else "  W  "
+            row += (" " + str(int(map[x][y])) + " ") if map[x][y] != "W" else "  W  "
         print(row)
-
-def getRewardMap(state):
-    """
-    return a 2D list representing the game state with reward values
-    """
-    # improvement: could make so that the map only checks a few cells in the game grid 
-    # and update instead of rebuilding the whole map over and over
-
-    # improvement: could store and reuse the map array from previous iterations 
-    # so that we dont have to rebuild base array each iteration
-    map = []
-
-    pacman_x, pacman_y = api.whereAmI(state)
-    walls = api.walls(state)
-    corners = api.corners(state)
-    ghosts = api.ghosts(state)
-    edible_ghost = dict(api.ghostStatesWithTimes(state))
-    food = api.food(state)
-    capsules = api.capsules(state)
-
-    max_x = 0
-    max_y = 0
-
-    for x,y in corners:
-        max_x = max(x, max_x)
-        max_y = max(y, max_y)
-
-    # build initial map with empty spaces for every position in grid
-    for _ in range(max_x+1):
-        map.append([VALUE["empty"]] * (max_y+1))
-
-    # place all game elements to the map
-    map[pacman_x][pacman_y] = VALUE["pacman"]
-    
-    for x,y in capsules:
-        map[x][y] = VALUE["capsule"]
-
-    for x,y in food:
-        map[x][y] = VALUE["food"]
-    
-    for x, y in walls:
-        map[x][y] = VALUE["wall"]
-    
-    for pos in ghosts:
-        # bug : ghosts position can be like x.5, y.5 but pacman's map array does not have indeces such as 1.5 
-        # therefore pacman's internal representation of where the ghosts are might be slightly wrong
-        # and this could lead to bugs/inaccuracies
-        x,y = util.nearestPoint(pos)
-        value = ((VALUE["edible_ghost"] * edible_ghost[pos])/40) if pos in edible_ghost and edible_ghost[pos] > 0 else VALUE["ghost"]
-
-        map[x][y] += value
-
-        for futureposition, steps in ghostFuture(pos, 5, map):
-            x,y = futureposition
-            map[x][y] += value * (1/steps)
-    
-    for x in range(8,12,1):
-        map[x][5] += VALUE["deathzone"]
-
-    return map
